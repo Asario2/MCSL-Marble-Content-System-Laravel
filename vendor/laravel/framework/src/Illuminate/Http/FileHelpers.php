d4425c4e97c4d413c20b85772208d1f960c7db24
Paul Rieser <161761197+Asario2@users.noreply.github.com>
2025-09-14 18:45:06 +0200

    Create FileHelpers.php

---
+++ vendor/laravel/framework/src/Illuminate/Http/FileHelpers.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Illuminate\Http;
+
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Auth;
+
+trait FileHelpers
+{
+    /**
+     * The cache copy of the file's hash name.
+     *
+     * @var string|null
+     */
+    protected $hashName = null;
+
+    /**
+     * Get the fully qualified path to the file.
+     *
+     * @return string
+     */
+    public function path()
+    {
+        return $this->getRealPath();
+    }
+
+    /**
+     * Get the file's extension.
+     *
+     * @return string
+     */
+    public function extension()
+    {
+        return $this->guessExtension();
+    }
+
+    /**
+     * Get a filename for the file.
+     *
+     * @param  string|null  $path
+     * @return string
+     */
+    public function hashName($path = null)
+    {
+        if ($path) {
+            $path = rtrim($path, '/').'/';
+        }
+
+        $hash = $this->hashName ?: $this->hashName = md5(basename($path."_".Auth::id()));
+
+        if ($extension = $this->guessExtension()) {
+            $extension = '.'.$extension;
+        }
+
+        return $path.$hash.$extension;
+    }
+
+    /**
+     * Get the dimensions of the image (if applicable).
+     *
+     * @return array|null
+     */
+    public function dimensions()
+    {
+        return @getimagesize($this->getRealPath());
+    }
+}
