3be8d43b51c20c58f92fb2fba28ce9d3912db0f6
Paul Rieser <161761197+Asario2@users.noreply.github.com>
2025-10-30 12:30:19 +0100

    Refactor NewHome.vue template structure

--- resources/js/Application/Homepage/ab/NewHome.vue
+++ resources/js/Application/Homepage/ab/NewHome.vue
@@ -1,59 +1,31 @@
 <template>
-<Layout>
-  <div itemscope itemtype="http://schema.org/Person">
-    <img :src="'/images/_ab/web/about-me.jpg'" alt="Paul Rieser" /><br />
-   <h3>Willkommen auf Asarios Blog – wo Kunst, Fotografie und Webdesign verschmelzen.</h3>
-      
-    Dieser Ort ist meine kreative Spielwiese: eine Sammlung von Bildern, Ideen und Designkonzepten, die zeigen, wie unterschiedlich Schönheit entstehen kann.<br /><br />
-    
-    Als Künstler und Webdesigner liebe ich es, mit Licht, Farbe und Form zu arbeiten – egal, ob auf Leinwand, in der Kamera oder im Code. <br /><br />
-    
-    Nimm dir Zeit, durch meine Werke zu stöbern und die Kunst hinter den Pixeln zu entdecken<br /><br />
-      
-    Ich bin <span itemprop="name">Paul Rieser</span>,
-    arbeite seit 1998 als <span itemprop="jobTitle">Webdesigner</span> und liebe es,
-    spannende Projekte umzusetzen. Mein größtes Abenteuer bisher war
-    <a href="https://www.asario.de" itemprop="url">www.asario.de</a> bei
-    <span itemprop="affiliation">MarbleFX</span>.<br><br />
+    <Layout>
+<div itemscope itemtype="http://schema.org/Person">
+  <img :src="'/images/_ab/web/about-me.jpg'" alt="Paul Rieser" itemprop="image" /><br />
 
-    Neben dem Webdesign bin ich als <span itemprop="alternateName">Asario</span> auch
-    künstlerisch unterwegs – Kunst, digitale Fotografie und Poesie gehören dazu.<br><br />
+  <h3 itemprop="headline">
+    Willkommen auf Asarios Blog – wo Kunst, Fotografie und Webdesign verschmelzen.
+  </h3>
 
-    <strong>Code & Chaos mit System:</strong> Ich tippe täglich HTML, CSS, JavaScript,
-    Vue.js, PHP/Laravel und jongliere mit Datenbanken. TailwindCSS, Ziggy oder Inertia?
-    Kein Problem – das ist mein Werkzeugkasten für schöne, funktionale Seiten.<br><br />
+  <p itemprop="description">
+    Dieser Ort ist meine kreative Spielwiese: eine Sammlung von Bildern, Ideen und
+    Designkonzepten, die zeigen, wie unterschiedlich Schönheit entstehen kann.
+  </p>
 
-    Hier gibt’s fast alles von mir:
-    <a href="home/pictures" itemprop="hasOccupation">Bilder</a> und
-    <a href="home/shortpoems" itemprop="hasOccupation">Gedichte</a>.<br><br />
+  <p>
+    Als <span itemprop="jobTitle">Künstler</span> und
+    <span itemprop="jobTitle">Webdesigner</span> liebe ich es, mit
+    <span itemprop="knowsAbout">Licht</span>,
+    <span itemprop="knowsAbout">Farbe</span> und
+    <span itemprop="knowsAbout">Form</span> zu arbeiten – egal, ob auf Leinwand,
+    in der Kamera oder im Code.
+  </p>
 
-    <div itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
-      Ich lebe zurzeit in <span itemprop="addressLocality">Bimöhlen</span>,
-      <span itemprop="addressCountry">SH</span>.<br><br />
-    </div>
-
-    Schreib mir: <span itemprop="email"><a href="mailto:paul.rieser@marblefx.net">paul.rieser[at]marblefx.net</a></span> oder
-    ruf an: <span itemprop="telephone"><a href='tel:+49 162 7716325'>+49 162 7716325</a></span><br /><br />
-
-    <!-- Persönliche Infos -->
-    <div itemprop="url" itemscope itemtype="http://schema.org/WebPage">
-      Mehr über mich bei <a href="/home/aboutme" itemprop="mainEntityOfPage">About Me</a>,<br /><br />
-      dort erfährst du, welche Musik bei mir läuft, wenn ich im Flow bin,
-      dass ich als Hobbys Zeichnen, Malen und digitale Fotografie habe und
-      Außerdem bin ich ein Freund von klaren Strukturen im Code und einem guten Scherz zwischendurch.
-    </div><br /><br />
-
-    <div class="flex justify-center mt-4">
-      <button
-        class="flex items-center gap-2 px-3 py-2 rounded-lg font-semibold bg-blue-500 text-white
-               hover:bg-blue-600 dark:bg-blue-600 dark:hover:bg-blue-700 dark:text-white
-               text-center tog-tab transition-all"
-        @click="redirect"
-      >
-        Zu meinem Blog
-      </button>
-    </div>
-  </div>
+  <p>
+    Nimm dir Zeit, durch meine Werke zu stöbern und die
+    <span itemprop="knowsAbout">Kunst hinter den Pixeln</span> zu entdecken.
+  </p>
+</div>
 </Layout>
 </template>
 
