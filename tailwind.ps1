d2c71d3be83d97852b63da86d43973b161fab65d
Paul Rieser <161761197+Asario2@users.noreply.github.com>
2025-11-15 14:15:48 +0100

    Delete tailwind.ps1

--- tailwind.ps1
+++
@@ -1,5 +0,0 @@
-php artisan app:convert-tailwind green.json nzl.config.js
-npx tailwindcss -c nzl.config.js -o public/css/tailw/nzl.css
-# php artisan css:clean-duplicates
-# php artisan css:clean-duplicates
-# php artisan css:clean-duplicates
