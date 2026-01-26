856ec11a8ec328a81a612270b321f046b04a3d2b
Asario2 <161761197+Asario2@users.noreply.github.com>
2026-01-26 16:45:38 +0100

    Update Versions

--- config/starter_eleven.php
+++ config/starter_eleven.php
@@ -4,7 +4,7 @@ return [
     'version' => [
         'brandname' => 'Asarios Blog',
         'copyrightname' => '© '.date("Y").' MCSL von Asario & Oliver Reinking',
-        'versionnr' => '4.40.3',
+        'versionnr' => '4.42.3',
         'versionsdatum' => '26.01.2026',
     ],
 ];
